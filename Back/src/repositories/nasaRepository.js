d8cf1b6b3b34c21fa68cd02d43857e1dfda9b9d1
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-08-10 17:59:05 -0500

    Correcion claves apis

--- Back/src/repositories/nasaRepository.js
+++ Back/src/repositories/nasaRepository.js
@@ -8,7 +8,7 @@ const NASA_API_KEY = process.env.NASA_API_KEY; // Obtén la clave de API desde l
 async function getImage() {
     try {
         // Realiza la solicitud a la API de la NASA usando la clave de API
-        const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=dIW9vbkMdfOG9TRQUMr6pVwU31U6MbBXUNmSpyyL`);
+        const response = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`);
         return response.data; // Devuelve los datos de la respuesta
     } catch (error) {
         throw new Error('Failed to fetch data from NASA API'); // Lanza un error si ocurre una excepción
